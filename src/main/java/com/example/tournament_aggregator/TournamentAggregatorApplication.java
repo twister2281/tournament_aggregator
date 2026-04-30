1619ad18c068ce9806a540f27140f4c115fc54a0
t.zaika-74259 <timaf5@yandex.ru>
2026-05-01 00:44:38 +0300

    Initial commit

---
+++ src/main/java/com/example/tournament_aggregator/TournamentAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tournament_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tournament_aggregator/TournamentAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tournament_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
